e3b609cd20bc6ff8206f883dbfab93f72483196e
Despina Ait-Amer <despinaaitamer@Despinas-MacBook-Air.local>
2024-09-13 14:37:43 +0100

    feat: xml data bound

--- app/src/main/java/com/example/databinding/MainActivity.java
+++ app/src/main/java/com/example/databinding/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.databinding.model.Person;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -21,4 +23,7 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    Person person = new Person();
+
 }
